6c64fb2fdf48818f88d057ccdfbb6b8660995724
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2026-06-06 06:25:15 +0200

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAlnum(char ch) {
+       return (ch >= '0' && ch <= '9') || 
+               (ch >= 'a' && ch <= 'z') || 
+               (ch >= 'A' && ch <= 'Z');
+
+        
+    }
+    public boolean isPalindrome(String s) {
+        int l = 0; 
+        int r = s.length() - 1;
+        s = s.toLowerCase();
+
+        while(l < r) {
+            char ch1 = s.charAt(l);
+            char ch2 = s.charAt(r);
+            if(!isAlnum(ch1)) {
+                l++;
+            }
+            else if(!isAlnum(ch2)) {
+                r--;
+            }
+            else {
+                if( ch1 != ch2) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
